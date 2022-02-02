a279fb662fa0c2f8114162a53c40d250be5e961e
Vaisakh Murali <mvaisakh@statixos.com>
2022-02-02 13:39:49 +0600

    MiuiCameraFramework: Initial stub Build class
    Change-Id: Iae535fe10a554ac71a3bc593adcd8d884f08d998
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ src/miui/os/Build.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2022 StatiX
+ * Copyright (C) 2022 Vaisakh Murali
+ */
+
+package miui.os;
+
+public final class Build extends android.os.Build {
+    public Build() {
+    }
+}
