8990362b061d90b4127226b7cf7735e0a92ce645
Anay Wadhera <anay1018@gmail.com>
2022-02-09 17:18:02 +0600

    MiuiCameraFramework: Implement international model check
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- src/miui/os/Build.java
+++ src/miui/os/Build.java
@@ -5,13 +5,20 @@
 
 package miui.os;
 
+import android.os.SystemProperties;
+
 public final class Build extends android.os.Build {
     public Build() {
     }
 
     public static final boolean IS_STABLE_VERSION = true;
-    public static final boolean IS_INTERNATIONAL_BUILD = true;
+    public static final boolean IS_INTERNATIONAL_BUILD = !isChinese();
     public static final boolean IS_ALPHA_BUILD = false;
     public static final boolean IS_CM_CUSTOMIZATION_TEST = false;
     public static final boolean IS_CM_CUSTOMIZATION = true;
+
+    public static boolean isChinese() {
+        String cnProp = SystemProperties.get("ro.boot.hwc");
+        return cnProp.equals("CN");
+    }
 }
